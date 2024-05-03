6e2ae9eb221d21bba5eb81e460354c53efab8d13
jonlecumberri <jonlecumberriarteta@gmail.com>
2024-05-03 17:46:48 +0200

    N1 and N2 computation

--- Project.py
+++ Project.py
@@ -35,6 +35,9 @@ images = load_images_from_folder(folder_image_path)
 im_001 = images["001.bmp"]
 
 plt.imshow(im_001[:,:,2])
+plt.axis("off")
+plt.show()
+
 
 def images_to_gray_scale(ims):
     images_gray = {}
@@ -42,8 +45,94 @@ def images_to_gray_scale(ims):
     for filename, image in ims.items():
         if i % 2 != 0:
             im_g = images[filename][:,:,0]
-            images_gray[filename] = im_g
+            images_gray[filename] = im_g / im_g.max()
         i = i + 1 
     return images_gray
         
 images_gray = images_to_gray_scale(images)
+
+
+im_001_gray = images_gray["001.bmp"]
+plt.imshow(im_001_gray, cmap = "gray")
+plt.axis("off")
+plt.show()
+
+
+#%%
+# =============================================================================
+# Threshold estimation for nucleus segmentation
+# =============================================================================
+
+maxIm = im_001_gray.max()
+minIm = im_001_gray.min()
+
+
+n1 = np.linspace(1,100,100)
+n2 = 3
+n2_2 = minIm * n1 / maxIm
+n2_3 = minIm / (1 - maxIm/n1)
+
+sensN1 =  (maxIm) / (maxIm + minIm * n1/n2_3)
+
+plt.plot(n1[0:10], -sensN1[0:10])
+plt.xlabel("n1")
+plt.ylabel("Υn1")
+plt.title("Sensivity for image 1")
+plt.show()
+
+for n in images_gray:
+    im = images_gray[n]
+    
+    maxIm = im.max()
+    minIm = im.min()
+    print(maxIm, minIm, "\n")
+    
+    n1 = np.linspace(1,100,100)
+    #n2 = 3
+    #n2_2 = minIm * n1 / maxIm
+    n2 = minIm / (1 - maxIm/n1)
+    
+    sensN1 =  (maxIm) / (maxIm + minIm * n1/n2)
+    
+    plt.plot(n1[0:10], -sensN1[0:10])
+ 
+plt.xlabel("n1")
+plt.ylabel("Υn1")
+plt.show()
+
+
+for n in images_gray:
+    im = images_gray[n]
+    
+    maxIm = im.max()
+    minIm = im.min()
+    
+    n2 = np.linspace(1,100,100)
+    #n2 = 3
+    #n2_2 = minIm * n1 / maxIm
+    n1 = maxIm / (1 - minIm/n2)
+    
+    sensN2 =  (minIm) / (minIm + maxIm * n2/n1)
+    
+    plt.plot(n2[0:10], -sensN2[0:10])
+ 
+plt.xlabel("n2")
+plt.ylabel("Υn2")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
